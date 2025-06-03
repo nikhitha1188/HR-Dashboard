6cc43c3775f86b2f09f65bf514dc29df5f84acc2
Nikhitha <166140758+nikhitha1188@users.noreply.github.com>
2025-06-03 14:19:16 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Ensures proper relative asset paths for deployment
+  base: './',
   plugins: [react()],
 })
